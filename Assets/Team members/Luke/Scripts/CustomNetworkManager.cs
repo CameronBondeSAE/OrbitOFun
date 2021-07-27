ed3b0e782a14e7d345a407bfb6c000c4aca97344
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-07-27 15:01:12 +1000

    NetworkManager prefab and script init

---
+++ Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Luke
+{
+    public class CustomNetworkManager : NetworkManager
+    {
+        //Variables
+
+        //References
+
+        //Events
+
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            base.OnServerAddPlayer(conn);
+
+
+        }
+    }
+}
