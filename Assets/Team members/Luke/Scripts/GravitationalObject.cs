ccb71f9f8f6f8e87f53f2e6579b4aceeb173c685
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-07-27 15:18:19 +1000

    Forgot namespaces

--- Assets/Team members/Luke/Scripts/GravitationalObject.cs
+++ Assets/Team members/Luke/Scripts/GravitationalObject.cs
@@ -2,57 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravitationalObject : MonoBehaviour
+namespace Luke
 {
-    //Variables
-    private Rigidbody rb;
-    public List<GravitationalObject> gravitationalObjects;
-    private float maxDistance;
-    private float distance;
-    private Vector3 direction;
-    private float forceMagnitude;
-    private Vector3 force;
-    private Rigidbody attractedRb;
-
-    //The gravitational constance
-    private float g = 50f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class GravitationalObject : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        gravitationalObjects.AddRange(FindObjectsOfType<GravitationalObject>());
-    }
+        //Variables
+        private Rigidbody rb;
+        public List<GravitationalObject> gravitationalObjects;
+        private float maxDistance;
+        private float distance;
+        private Vector3 direction;
+        private float forceMagnitude;
+        private Vector3 force;
+        private Rigidbody attractedRb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        GravityToAllObj();
-    }
+        //The gravitational constance
+        private float g = 50f;
 
-    public void Attract(GravitationalObject objectToAttract)
-    {
-        attractedRb = objectToAttract.rb;
-        
-        direction = transform.position - attractedRb.position;
-        distance = direction.magnitude;
-        
-        //newtons universal law of gravity
-        forceMagnitude = g * (rb.mass * attractedRb.mass) / Mathf.Pow(distance, 2);
-        
-        force = direction.normalized * forceMagnitude;
-        attractedRb.AddForce(force);
-    }
-
-    public void GravityToAllObj()
-    {
-        foreach (GravitationalObject obj in gravitationalObjects)
+        // Start is called before the first frame update
+        void Start()
         {
-            //so it doesn't attract itself
-            if( obj != this)
+            rb = GetComponent<Rigidbody>();
+            gravitationalObjects.AddRange(FindObjectsOfType<GravitationalObject>());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GravityToAllObj();
+        }
+
+        public void Attract(GravitationalObject objectToAttract)
+        {
+            attractedRb = objectToAttract.rb;
+
+            direction = transform.position - attractedRb.position;
+            distance = direction.magnitude;
+
+            //newtons universal law of gravity
+            forceMagnitude = g * (rb.mass * attractedRb.mass) / Mathf.Pow(distance, 2);
+
+            force = direction.normalized * forceMagnitude;
+            attractedRb.AddForce(force);
+        }
+
+        public void GravityToAllObj()
+        {
+            foreach (GravitationalObject obj in gravitationalObjects)
             {
-                Attract(obj);
+                //so it doesn't attract itself
+                if (obj != this)
+                {
+                    Attract(obj);
+                }
             }
         }
+
     }
-}
--- Assets/Team members/Luke/Scripts/PlanetManager.cs
+++ Assets/Team members/Luke/Scripts/PlanetManager.cs
@@ -2,38 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetManager : MonoBehaviour
+namespace Luke
 {
-    public List<GravitationalObject> gravitationalObjects;
-    
-    public float maxPosVelocity;
-    public float minNegVelocity;
-    private float xVelocity;
-    private float yVelocity;
-    private float zVelocity;
-    private Rigidbody currentRb;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlanetManager : MonoBehaviour
     {
-        gravitationalObjects.AddRange(FindObjectsOfType<GravitationalObject>());
-        ApplyRandomVelocity();
-    }
+        public List<GravitationalObject> gravitationalObjects;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public float maxPosVelocity;
+        public float minNegVelocity;
+        private float xVelocity;
+        private float yVelocity;
+        private float zVelocity;
+        private Rigidbody currentRb;
 
-    public void ApplyRandomVelocity()
-    {
-        foreach (GravitationalObject obj in gravitationalObjects)
+        // Start is called before the first frame update
+        void Start()
         {
-            xVelocity = Random.Range(-minNegVelocity, maxPosVelocity);
-            yVelocity = Random.Range(-minNegVelocity, maxPosVelocity);
-            zVelocity = Random.Range(-minNegVelocity, maxPosVelocity);
-            obj.GetComponent<Rigidbody>().velocity = new Vector3(xVelocity,yVelocity,zVelocity);
+            gravitationalObjects.AddRange(FindObjectsOfType<GravitationalObject>());
+            ApplyRandomVelocity();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ApplyRandomVelocity()
+        {
+            foreach (GravitationalObject obj in gravitationalObjects)
+            {
+                xVelocity = Random.Range(-minNegVelocity, maxPosVelocity);
+                yVelocity = Random.Range(-minNegVelocity, maxPosVelocity);
+                zVelocity = Random.Range(-minNegVelocity, maxPosVelocity);
+                obj.GetComponent<Rigidbody>().velocity = new Vector3(xVelocity, yVelocity, zVelocity);
+            }
         }
     }
 }
