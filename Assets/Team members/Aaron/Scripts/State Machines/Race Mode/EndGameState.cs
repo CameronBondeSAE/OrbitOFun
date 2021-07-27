229740809cee67b3b3e0f8b7b38e8f51ad0686fe
AaronMcDougall <1021572@student.sae.edu.au>
2021-07-27 15:30:16 +1000

    Race state Skeleton

---
+++ Assets/Team members/Aaron/Scripts/State Machines/Race Mode/EndGameState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace AaronMcDougall
+{
+    public class EndGameState : RaceModeStateBase
+    {
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+    }
+
+}
---
+++ Assets/Team members/Aaron/Scripts/State Machines/Race Player/DeathState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Remoting.Services;
+
+namespace AaronMcDougall
+{
+    public class DeathState : RacePlayerStateBase
+    {
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+    }
+}
