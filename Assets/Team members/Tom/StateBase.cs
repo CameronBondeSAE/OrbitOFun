bdfee4dfc9eb10726e329cc79d0691836b001b27
Tom Bartle <1022049@student.sae.edu.au>
2021-07-27 15:06:52 +1000

    Battle Player States skeleton code

---
+++ Assets/Team members/Tom/StateBase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tom
+{
+    public class StateBase : MonoBehaviour
+    {
+        public virtual void Enter()
+        {
+            
+        }
+
+        public virtual void Execute()
+        {
+            
+        }
+
+        public virtual void Exit()
+        {
+            
+        }
+    }
+}
