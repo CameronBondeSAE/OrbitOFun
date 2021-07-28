c807fd6041d807557ecc7c0149e0e3289c7573e1
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-07-28 15:01:23 +1000

    ParrallelSync init

--- Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
+++ Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
@@ -15,7 +15,17 @@ namespace Luke
 
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
-            base.OnServerAddPlayer(conn);
+            //base.OnServerAddPlayer(conn);
+            
+            Transform startPos = GetStartPosition();
+            GameObject player = startPos != null
+                ? Instantiate(playerPrefab, startPos.position, startPos.rotation)
+                : Instantiate(playerPrefab);
+
+            // instantiating a "Player" prefab gives it the name "Player(clone)"
+            // => appending the connectionId is WAY more useful for debugging!
+            player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
+            NetworkServer.AddPlayerForConnection(conn, player);
         }
     }
 }
