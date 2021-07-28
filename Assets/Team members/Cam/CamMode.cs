00f1ae9c7d5d2a6c8adfd732f8b3ec34345a4dd3
Cameron Bonde <c.bonde@sae.edu>
2021-07-28 16:39:25 +1000

    Core scene with managers + Main test scene additive loading

---
+++ Assets/Team members/Cam/CamMode.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zach;
+using Luke;
+using System.Linq;
+
+public class CamMode : GameModeBase
+{
+	public List<GravitationalObject> gravObjects;
+	
+	public void Start()
+	{
+		// gravObjects = new List<GravitationalObject>();
+	}
+	
+	public void DoThing()
+	{
+		gravObjects = FindObjectsOfType<GravitationalObject>().ToList();
+	}
+	
+	public override void Activate()
+	{
+		base.Activate();
+		
+		Debug.Log("CamMode Activate");
+	}
+}
