0c49175eac7dcf2005aad56e8c444a5124f61c68
Riley McGowan <1017093@student.sae.edu.au>
2021-08-04 16:47:08 +1000

    Mass and Size Increase Damage

--- Assets/Team members/Riley/Scripts/DamageOnHit.cs
+++ Assets/Team members/Riley/Scripts/DamageOnHit.cs
@@ -7,13 +7,26 @@ namespace RileyMcGowan
 {
     public class DamageOnHit : MonoBehaviour
     {
+        [Tooltip("Should the object hit increase mass")]
+        public bool increaseMass;
+        [Tooltip("Should the object hit increase size")]
+        public bool increaseSize;
+        
         private void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.GetComponent<Gravity>() != null)
             {
-                if (gameObject.GetComponent<CanGravitate>())
+                if (gameObject.GetComponent<CanGravitate>() != null)
                 {
                     gameObject.GetComponent<CanGravitate>().OnDestroy();
+                    if (increaseMass == true)
+                    {
+                        other.gameObject.GetComponent<Rigidbody>().mass += GetComponent<Rigidbody>().mass/5f;
+                    }
+                    if (increaseSize == true)
+                    {
+                        other.gameObject.transform.localScale += gameObject.transform.localScale/30f;
+                    }
                     Destroy(gameObject);
                 }
             }
