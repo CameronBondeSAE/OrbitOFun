ecef19364e79224120afc54b057aecc7c6b701cf
Rob <1021549@student.sae.edu.au>
2021-08-04 10:40:45 +1000

    bezier scene

--- Assets/Team members/Rob/BezierCurve/BezierCurve.cs
+++ Assets/Team members/Rob/BezierCurve/BezierCurve.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using UnityEngine;
 
 public class BezierCurve : MonoBehaviour
