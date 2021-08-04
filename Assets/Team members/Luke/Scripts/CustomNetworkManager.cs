bdc7881498feee622c30e6b7117aaad268715be5
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-08-04 15:16:08 +1000

    Update CustomNetworkManager.cs

--- Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
+++ Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
@@ -9,29 +9,18 @@ namespace LukeBaker
     {
         //Variables
         // TODO needs to reference each game modes starting positions??
-        public List<Transform> startingPositions;
-        public List<NetworkConnection> players;
-
-        //References
-        // TODO lobby ref
-
-        //Events
-        
-
-        public override void OnStartServer()
-        {
-            base.OnStartServer();
-        }
+        public List<Transform> playerStartingPositions;
+        public List<NetworkConnection> playersConnected;
 
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
-            players.Add(conn);
+            playersConnected.Add(conn);
         }
 
         public void OnServerSpawnPlayers(NetworkConnection conn)
         {
             //start game button or event???
-            foreach (NetworkConnection connection in players)
+            foreach (NetworkConnection connection in playersConnected)
             {
                 Transform startPos = GetStartPosition();
                 GameObject player = startPos != null
@@ -40,8 +29,8 @@ namespace LukeBaker
 
                 // instantiating a "Player" prefab gives it the name "Player(clone)"
                 // => appending the connectionId is WAY more useful for debugging!
-                player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
-                NetworkServer.AddPlayerForConnection(conn, player);
+                player.name = $"{playerPrefab.name} [connId={connection.connectionId}]";
+                NetworkServer.AddPlayerForConnection(connection, player);
             }
         }
     }
