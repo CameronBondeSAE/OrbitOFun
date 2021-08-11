ac00b8074e0a516f7a4f8262e425a463a8be82a3
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-08-11 14:30:17 +1000

    commented lobbied players on networkman

--- Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
+++ Assets/Team members/Luke/Scripts/CustomNetworkManager.cs
@@ -23,14 +23,14 @@ namespace LukeBaker
         {
             base.OnServerConnect(conn);
             playerIP.Add(conn.address);
-            lobbiedPlayers.Add(conn);
+            //lobbiedPlayers.Add(conn);
         }
         
         public override void OnServerDisconnect(NetworkConnection conn)
         {
             base.OnServerDisconnect(conn);
             playerIP.Remove(conn.address);
-            lobbiedPlayers.Remove(conn);
+            //lobbiedPlayers.Remove(conn);
         }
 
         //TODO to be called by gamemodes
