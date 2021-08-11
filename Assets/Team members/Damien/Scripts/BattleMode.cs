565ec776f22f4dfa0d47fe1947d890048b3c0a47
Damien Harwood <1019243@student.sae.edu.au>
2021-08-11 16:07:52 +1000

    battlemode skeleton

--- Assets/Team members/Damien/Scripts/BattleMode.cs
+++ Assets/Team members/Damien/Scripts/BattleMode.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zach;
 
-public class BattleMode : MonoBehaviour
+public class BattleMode : GameModeBase
 {
     // Start is called before the first frame update
     void Start()
