f96273d5733e43734348b9f569aded955a358000
Venomlord123 <1019559@student.sae.edu.au>
2021-08-11 12:37:04 +1000

    Level Updated and Material/Shader Progress

---
+++ Assets/Team members/Zach/Scripts/GravityManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Zach
+{
+    public class GravityManager : MonoBehaviour
+    {
+        //Todo maybe change gravity to be managed by this instead of the themselves
+        
+        public List<GameObject> gravityObjects;
+        public Gravity gravity;
+
+        
+    }
+}
