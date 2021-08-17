8895470eef70823f10c97dd156dc80e99bb960aa
Venomlord123 <1019559@student.sae.edu.au>
2021-08-17 16:40:52 +1000

    Black hole works

--- Assets/Team members/Zach/Scripts/BlackHole.cs
+++ Assets/Team members/Zach/Scripts/BlackHole.cs
@@ -8,9 +8,19 @@ namespace Zach
 {
     public class BlackHole : MonoBehaviour
     {
+        public Vector3 gizmosSize;
+        public float xLength;
+        public float yHeight;
+        
         private void OnCollisionEnter(Collision other)
         {
-            other.transform.position = new Vector3(Random.Range(0f, 10f), Random.Range(0f, 10f), 0);
+            other.transform.position = new Vector3(Random.Range(-xLength/2f, xLength/2f), Random.Range(-yHeight/2f, yHeight/2f), 0);
+        }
+
+        private void OnDrawGizmos()
+        {
+            gizmosSize = new Vector3(xLength, yHeight);
+            Gizmos.DrawCube(transform.position,gizmosSize);
         }
     }
 }
