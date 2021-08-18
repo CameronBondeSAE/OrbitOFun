c1290dca3ab3ff9f072894cb3281659b34ff931d
Cameron Bonde <c.bonde@sae.edu>
2021-08-18 18:44:28 +1000

    no message

--- Assets/Tutorials and examples/Rotatey.cs
+++ Assets/Tutorials and examples/Rotatey.cs
@@ -3,6 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotatey : NetworkBehaviour
+
+public class RotateyBase : NetworkBehaviour
+{
+	
+}
+
+public class Rotatey : RotateyBase
 {
 }
