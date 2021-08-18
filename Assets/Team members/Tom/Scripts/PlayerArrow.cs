23743918b0ed652902b350d1176dffe32da10621
Damien Harwood <1019243@student.sae.edu.au>
2021-08-18 15:28:57 +1000

    Arrow Networked

--- Assets/Team members/Tom/Scripts/PlayerArrow.cs
+++ Assets/Team members/Tom/Scripts/PlayerArrow.cs
@@ -1,51 +1,86 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Tom
 {
-    public class PlayerArrow : MonoBehaviour
+    public class PlayerArrow : NetworkBehaviour
     {
         public float rotationSpeed = 180f; // How many degrees per second
         public float rotationLimit = 45f;
         private float startingAngle;
+        private ZachsPlayerActions zachsPlayerActions;
+        public Vector3 rotateVelocity;
         
         private float direction; // Set this to the input axis
 
-        private void Start()
+        
+        public override void OnStartLocalPlayer()
         {
+            base.OnStartLocalPlayer();
             startingAngle = transform.eulerAngles.z;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+            if (isClient)
             {
-                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
-            }
+                if (InputSystem.GetDevice<Keyboard>().aKey.isPressed)
+                {
+                    transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+                }
 
-            if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
-            {
-                transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
+                if (InputSystem.GetDevice<Keyboard>().dKey.isPressed)
+                {
+                    transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
+                }
+
+
+                // Replace above with this function when using new Input System
+                // Set direction to axis input
+                //transform.Rotate(Vector3.forward * rotationSpeed * direction * Time.deltaTime);
+
+                float currentRotation = transform.eulerAngles.z;
+                // HACK
+                // Counters eulerAngles clamping between 0 and 360
+                if (currentRotation > 180f && startingAngle - rotationLimit < 0)
+                {
+                    currentRotation -= 360f;
+                }
+
+                currentRotation = Mathf.Clamp(currentRotation, startingAngle - rotationLimit,
+                    startingAngle + rotationLimit);
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentRotation);
             }
-            
-            // Replace above with this function when using new Input System
-            // Set direction to axis input
-            //transform.Rotate(Vector3.forward * rotationSpeed * direction * Time.deltaTime);
-            
-            float currentRotation = transform.eulerAngles.z;
-            // HACK
-            // Counters eulerAngles clamping between 0 and 360
-            if (currentRotation > 180f && startingAngle - rotationLimit < 0)
-            {
-                currentRotation -= 360f;
-            }
-            currentRotation = Mathf.Clamp(currentRotation, startingAngle - rotationLimit,
-                startingAngle + rotationLimit);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentRotation);
+        }
+
+
+        public void EnableControls()
+        {
+            //todo refactor to work with system wide new input system
+            zachsPlayerActions = new ZachsPlayerActions();
+            zachsPlayerActions.Enable();
+
+            zachsPlayerActions.GeneralMovement.Rotate.started += RotateOnPerformed;
+            zachsPlayerActions.GeneralMovement.Rotate.canceled += RotateOnPerformed;
+        }
+
+        public void DisableControls()
+        {
+            //todo refactor to work with system wide new input system
+            zachsPlayerActions = new ZachsPlayerActions();
+            zachsPlayerActions.Enable();
+
+            zachsPlayerActions.GeneralMovement.Rotate.started -= RotateOnPerformed;
+            zachsPlayerActions.GeneralMovement.Rotate.canceled -= RotateOnPerformed;
+        }
+        private void RotateOnPerformed(InputAction.CallbackContext obj)
+        {
+            rotateVelocity = new Vector3(0, 0, -15 * obj.ReadValue<float>());
         }
 
         public Vector3 GetEulerAngles()
