7301446c3ee95f84dfc17b173f77fc625492447c
Rob <1021549@student.sae.edu.au>
2021-08-24 14:48:08 +1000

    camera stuff

--- Assets/Team members/Rob/BezierCurve/CameraTracking.cs
+++ Assets/Team members/Rob/BezierCurve/CameraTracking.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraTracking : MonoBehaviour
+public class CameraTracking : CameraBase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public Transform shipTransform;
+   public float speed;
+   public Vector3 offSet;
+   public float velocityMultiplier;
+
+
+   public override void AssignTarget(Transform target)
+   {
+      base.AssignTarget(target);
+      shipTransform = target;
+
+   }
+
+   private void FixedUpdate()
+   {
+      Vector3 cameraPos = shipTransform.position + offSet;
+      Vector3 smoothPos = Vector3.Lerp(transform.position, cameraPos, Time.deltaTime * speed);
+      transform.position = smoothPos;
+   }
+
+   /*private void LateUpdate()
+   {
+      float xVel = shipTransform.GetComponent<Rigidbody>().velocity.x * velocityMultiplier;
+   }*/
+
+   /*private void Update()
+   {
+      if (shipTransform != null)
+      {
+         transform.position = Vector2.Lerp(transform.position, shipTransform.position, speed);
+      }
+   }*/
 }
