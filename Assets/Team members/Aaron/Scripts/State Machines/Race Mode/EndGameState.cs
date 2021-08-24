9393b08c1cb84a2ca8e0df81b103ab544a803201
Tom Bartle <1022049@student.sae.edu.au>
2021-08-24 16:14:10 +1000

    Race mode end game state logic

--- Assets/Team members/Aaron/Scripts/State Machines/Race Mode/EndGameState.cs
+++ Assets/Team members/Aaron/Scripts/State Machines/Race Mode/EndGameState.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Mirror;
 
 namespace AaronMcDougall
 {
     public class EndGameState : StateBase
     {
+        public GameObject endGameUIPrefab;
+        private GameObject spawnedUI;
+        public float showUITime = 10f;
+        
         public override void Enter()
         {
             base.Enter();
 
             //Show winner message, player username
             //enter RaceEndState?
+            
+            SpawnEndGameUI();
+            StartCoroutine(ShowUICountdown());
         }
 
         public override void Execute()
         {
             base.Execute();
+            
         }
 
         public override void Exit()
@@ -25,6 +34,26 @@ namespace AaronMcDougall
             base.Exit();
             
             //return to main menu/mode select/enter PreTimerState
+            
+            DestroyUI();
+        }
+
+        public void SpawnEndGameUI()
+        {
+            spawnedUI = Instantiate(endGameUIPrefab);
+            NetworkServer.Spawn(spawnedUI);
+            //spawnedUI.GetComponent<EndGameUI>().SetWinnerName(//Get winner player name);
+        }
+
+        public void DestroyUI()
+        {
+            Destroy(spawnedUI.gameObject);
+        }
+
+        public IEnumerator ShowUICountdown()
+        {
+            yield return new WaitForSeconds(showUITime);
+            GetComponent<RaceModeStateManager>().ChangeState(GetComponent<PreTimerState>());
         }
     }
 
