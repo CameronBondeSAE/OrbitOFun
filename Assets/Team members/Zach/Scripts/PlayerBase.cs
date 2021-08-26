dd6ae0c7f93d35fbbca35c850355eb03fd5793bb
Cam <c.bonde@sae.edu>
2021-08-26 18:53:42 +1000

    no message

--- Assets/Team members/Zach/Scripts/PlayerBase.cs
+++ Assets/Team members/Zach/Scripts/PlayerBase.cs
@@ -7,92 +7,96 @@ using UnityEngine.VFX;
 
 namespace Zach
 {
-    public class PlayerBase : NetworkBehaviour
-    {
-        public Rigidbody rigidBody;
-        public Vector3 rotateVelocity;
-        public float speed;
-        public float maxSpeed;
+	public class PlayerBase : NetworkBehaviour
+	{
+		public Rigidbody rigidBody;
+		public Vector3   rotateVelocity;
+		public float     speed;
+		public float     maxSpeed;
 
-        public float forwardFloat;
-        private ZachsPlayerActions zachsPlayerActions;
-        public VisualEffect particle;
-        
-        public void Awake()
-        {            
-            zachsPlayerActions = new ZachsPlayerActions();
-            zachsPlayerActions.Enable();
-            particle.Stop();
+		public  float              forwardFloat;
+		private ZachsPlayerActions zachsPlayerActions;
+		public  VisualEffect       particle;
 
-            // Interact with Health events
-            GetComponent<Health>().deathEvent += OnDeathEvent;
-        }
+		public void Awake()
+		{
+			zachsPlayerActions = new ZachsPlayerActions();
+			zachsPlayerActions.Enable();
+			particle.Stop();
 
-        void OnDestroy()
-        {
-            GetComponent<Health>().deathEvent -= OnDeathEvent;
-            RpcDisableControls();
-        }
+			// Interact with Health events
+			GetComponent<Health>().deathEvent += OnDeathEvent;
+		}
 
-        void OnDeathEvent(Health obj)
-        {
-            // Destroy(gameObject);
-            // TODO: Turn off on client probably
-            // gameObject.SetActive(false);
-            RpcDisableControls();
-        }
+		void OnDestroy()
+		{
+			GetComponent<Health>().deathEvent                  -= OnDeathEvent;
+			zachsPlayerActions.GeneralMovement.Move.started    -= Movement;
+			zachsPlayerActions.GeneralMovement.Move.canceled   -= Movement;
+			zachsPlayerActions.GeneralMovement.Rotate.started  -= RotateOnPerformed;
+			zachsPlayerActions.GeneralMovement.Rotate.canceled -= RotateOnPerformed;
+		}
 
-        [ClientRpc]
-        public void RpcEnableControls()
-        {
-            //todo refactor to work with system wide new input system
-            
-            zachsPlayerActions.GeneralMovement.Move.started += Movement;
-            zachsPlayerActions.GeneralMovement.Move.canceled += Movement;
-            zachsPlayerActions.GeneralMovement.Rotate.started += RotateOnPerformed;
-            zachsPlayerActions.GeneralMovement.Rotate.canceled += RotateOnPerformed;
-        }
+		void OnDeathEvent(Health obj)
+		{
+			// Destroy(gameObject);
+			// TODO: Turn off on client probably
+			// gameObject.SetActive(false);
+			if (isServer)
+				RpcDisableControls();
+		}
 
-        [ClientRpc]
-        public void RpcDisableControls()
-        {
-            //todo refactor to work with system wide new input system
-         
-            zachsPlayerActions.GeneralMovement.Move.started -= Movement;
-            zachsPlayerActions.GeneralMovement.Move.canceled -= Movement;
-            zachsPlayerActions.GeneralMovement.Rotate.started -= RotateOnPerformed;
-            zachsPlayerActions.GeneralMovement.Rotate.canceled -= RotateOnPerformed;
-        }
+		[ClientRpc]
+		public void RpcEnableControls()
+		{
+			//todo refactor to work with system wide new input system
 
-        public void FixedUpdate()
-        {
-            if (isLocalPlayer)
-            {
-                rigidBody.AddRelativeForce(Vector3.up * speed * forwardFloat);
-                rigidBody.angularVelocity = rotateVelocity;
-                // if (rigidBody.velocity.magnitude >= maxSpeed)
-                    // rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
-            }
-        }
-        
+			zachsPlayerActions.GeneralMovement.Move.started    += Movement;
+			zachsPlayerActions.GeneralMovement.Move.canceled   += Movement;
+			zachsPlayerActions.GeneralMovement.Rotate.started  += RotateOnPerformed;
+			zachsPlayerActions.GeneralMovement.Rotate.canceled += RotateOnPerformed;
+		}
 
-        private void Movement(InputAction.CallbackContext obj)
-        {
-            //velocity = new Vector3(0, 0, speed * obj.ReadValue<float>());
-            forwardFloat = obj.ReadValue<float>();
-            if (obj.ReadValue<float>() > 0)
-            {
-                particle.Play();
-            }
-            else
-            {
-                particle.Stop();
-            }
-        }
+		[ClientRpc]
+		public void RpcDisableControls()
+		{
+			//todo refactor to work with system wide new input system
 
-        private void RotateOnPerformed(InputAction.CallbackContext obj)
-        {
-            rotateVelocity = new Vector3(0, 0, -15 * obj.ReadValue<float>());
-        }
-    }
+			zachsPlayerActions.GeneralMovement.Move.started    -= Movement;
+			zachsPlayerActions.GeneralMovement.Move.canceled   -= Movement;
+			zachsPlayerActions.GeneralMovement.Rotate.started  -= RotateOnPerformed;
+			zachsPlayerActions.GeneralMovement.Rotate.canceled -= RotateOnPerformed;
+		}
+
+		public void FixedUpdate()
+		{
+			if (isLocalPlayer)
+			{
+				rigidBody.AddRelativeForce(Vector3.up * speed * forwardFloat);
+				rigidBody.angularVelocity = rotateVelocity;
+				// if (rigidBody.velocity.magnitude >= maxSpeed)
+				// rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+			}
+		}
+
+
+		private void Movement(InputAction.CallbackContext obj)
+		{
+			//velocity = new Vector3(0, 0, speed * obj.ReadValue<float>());
+			forwardFloat = obj.ReadValue<float>();
+			if (obj.ReadValue<float>() > 0)
+			{
+				particle.Play();
+			}
+			else
+			{
+				particle.Stop();
+			}
+		}
+
+		private void RotateOnPerformed(InputAction.CallbackContext obj)
+		{
+			rotateVelocity = new Vector3(0, 0, -15 * obj.ReadValue<float>());
+		}
+	}
 }
