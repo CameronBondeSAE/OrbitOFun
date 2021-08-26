660422a9b1e126faee69bd86b8f1c4ee0709626e
Tom Bartle <1022049@student.sae.edu.au>
2021-08-26 10:59:13 +1000

    Added level for battle mode

--- Assets/Team members/Tom/Scripts/SlowField.cs
+++ Assets/Team members/Tom/Scripts/SlowField.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror.Examples.Chat;
 using UnityEngine;
 using Zach;
 
@@ -11,24 +12,34 @@ namespace Tom
         public float slowSpeed = 1f;
         private float storedSpeed;
         
-        private void OnTriggerEnter(Collider other)
+        // private void OnTriggerEnter(Collider other)
+        // {
+        //     PlayerBase player = other.GetComponentInParent<PlayerBase>();
+        //
+        //     if (player != null)
+        //     {
+        //         storedSpeed = player.maxSpeed;
+        //         player.maxSpeed = slowSpeed;
+        //     }
+        // }
+        //
+        // private void OnTriggerExit(Collider other)
+        // {
+        //     PlayerBase player = other.GetComponentInParent<PlayerBase>();
+        //
+        //     if (player != null)
+        //     {
+        //         player.maxSpeed = storedSpeed;
+        //     }
+        // }
+
+        private void OnTriggerStay(Collider other)
         {
             PlayerBase player = other.GetComponentInParent<PlayerBase>();
-
             if (player != null)
             {
-                storedSpeed = player.maxSpeed;
-                player.maxSpeed = slowSpeed;
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            PlayerBase player = other.GetComponentInParent<PlayerBase>();
-
-            if (player != null)
-            {
-                player.maxSpeed = storedSpeed;
+                player.GetComponent<Rigidbody>().velocity =
+                    player.GetComponent<Rigidbody>().velocity.normalized * slowSpeed;
             }
         }
     }
